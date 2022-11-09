5322fc998cb7f61399d5458ba7aba81a10ba3d78
Тетяна Федорків <tetanafedorkiv@MacBook-Air-Tetana.local>
2022-11-09 22:01:39 +0200

    homework2

---
+++ hw2/script.js
@@ -0,0 +1,71 @@
+// Hello world!
+
+let firstVariable;
+
+firstVariable = 'Hello World!';
+
+console.log(firstVariable);
+
+let userName = 'Tania';
+
+console.log(userName)
+
+let width, height, length;
+
+let maxSpeed = 200,
+    minSpeed = 40;
+
+let button = 'button';
+let form;
+let input;
+
+let myHobby = 'fishing';
+// let myHobby = 'painting';
+
+console.log(myHobby)
+
+let childAge = 12;
+// ...
+childAge = 13;
+console.log(childAge);
+
+let rectangleWidth = 200;
+
+let rectangleHeight = rectangleWidth;
+
+console.log(rectangleHeight)
+
+let user;
+let userSurname;
+let dinoAge;
+let info1;
+
+let operationResult;
+let operationresult;
+// різні змінні
+
+let $user;
+let _log;
+// і таке можливо
+
+// let 1us;
+// let pet name;
+// let dog-breed;
+// крінж. не писати так!
+
+let привітання = 'Hello darling!';
+console.log(привітання);
+// можна й так, не рекомендовано бо весь синтаксис джес викор латинку
+
+// let let;
+// let const;
+// let for;
+// зарезервовані слова, незя використ
+
+const clientNickname = 'user42';
+
+clientNickname = 'user24';
+
+// const empty;
+
+
