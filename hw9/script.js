0741a697f506628b021ed76a96e4493873bc120f
Тетяна Федорків <tetanafedorkiv@MacBook-Air-Tetana.local>
2022-12-06 22:32:38 +0200

    homework

--- hw9/script.js
+++ hw9/script.js
@@ -47,3 +47,78 @@ const filterAdultAdmin = (dataArr) => {
   
   const adultAdmins = filterAdultAdmin(users); // [Sam, Pablo]
   console.log(adultAdmins)
+
+// ex 1.3
+
+const countAverage = (arr) => {
+    return arr.reduce((total, amount, index, array) => {
+        total += amount;
+        if(index === array.length - 1) {
+            return total / array.length;
+        }else {
+            return total;
+        }
+    });
+}
+
+const averageResult = countAverage([1, 10, 12, 33, 62])
+console.log(averageResult)
+
+// ex 2 
+
+const isNegative = (number) => number < 0;
+const increment = (number) => number + 1;
+const logger = (element, index, array) => {
+    console.log(`In array [${array}] on position ${index}: ${element}`);
+}
+
+const ownForEach = (arr, logger) => {
+
+    for(let num of arr){
+        logger(num, arr.indexOf(num), arr)
+    }
+
+}
+
+ownForEach([1, 2, 3], logger)
+
+let arr = [1, 2, 3]
+let test = arr.forEach((num) => logger(num, arr.indexOf(num), arr))
+
+const ownMap = (arr, increment) => {
+    let arrClone = []
+
+    for(let num of arr){
+        arrClone.push(increment(num))
+    }
+
+    return arrClone
+}
+
+let resOwnMap = ownMap([1, 2, 3], increment)
+console.log(resOwnMap)
+
+let arr2 = [1, 2, 3]
+
+let test1 = arr1.map((num))
+console.log(test1)
+
+const ownFilter = (arr, isNegative) => {
+
+    let arrClone = []
+
+    for(let num of arr){
+
+        if(isNegative(num) === true){
+
+            arrClone.push(num)
+        }
+    }
+    return arrClone
+}
+
+let resOwnFilter = ownFilter([-2, 5, -1], isNegative);
+console.log(resOwnFilter)
+
+let arr3 = [-2, 5, 6]
+// let test2 = arr3.filter(num) => isNegative(num)
